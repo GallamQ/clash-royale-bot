46db13f1c0ee58ab19b1ab544c375eeab9ad7118
GallamQ <quentindetre@yahoo.com>
2026-09-20 14:09:11 +0000

    Add database pool and connection check script

---
+++ src/scripts/db-check.ts
@@ -0,0 +1,16 @@
+import { pool } from "../db/pool";
+
+async function main() {
+  const info = await pool.query("SELECT now() AS now, current_user AS user");
+  console.log(info.rows[0]);
+
+  const count = await pool.query("SELECT count(*) FROM clan_members");
+  console.log(count.rows[0]);
+
+  await pool.end();
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
